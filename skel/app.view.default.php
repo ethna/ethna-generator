77a2ad7350103b821393a2ea08745c3e9b42a671
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2014-02-08 01:38:01 +0900

    import from ethna-2.6.0-beta4

---
+++ skel/app.view.default.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *  Index.php
+ *
+ *  @author     {$author}
+ *  @package    {$project_id}
+ *  @version    $Id: 213d11c251104242f9806527d410cf7dad779c54 $
+ */
+
+/**
+ *  Index view implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    {$project_id}
+ */
+class {$project_id}_View_Index extends {$project_id}_ViewClass
+{
+    /**
+     *  preprocess before forwarding.
+     *
+     *  @access public
+     */
+    public function preforward()
+    {
+    }
+}
+
